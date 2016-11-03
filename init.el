ef720da49bbdb54c65400955441249577b88ec76
Vivian Lim <vivlim@microsoft.com>
2016-11-03 14:53:10 -0700

    Add evil-snipe

--- init.el
+++ init.el
@@ -58,6 +58,11 @@ values."
      vim-empty-lines
      vinegar
      react
+     (evil-snipe :variables
+                 evil-snipe-enable-alternate-f-and-t-behaviors t
+                 evil-snipe-scope 'buffer
+                 evil-snipe-repeat-scope 'buffer
+                 )
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
